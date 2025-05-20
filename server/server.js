0dcf01276308c974b9ca1ddfdefcfb4b540c3e7b
preet sahu <sahupreet2001@gmail.com>
2025-05-20 13:00:48 +0530

    changes port

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:4174", "http://localhost:4173"];
 
 app.use(
   cors({
